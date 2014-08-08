92c5898b89e96ba4fa4b61f9b8787cd884ec884a
srhuevo <garoz.daniel@gmail.com>
2014-08-08 16:59:29 +0200

    first commit

---
+++ mail_tuit/src/d/garoz/main/mainMailTuit.java
@@ -0,0 +1,5 @@
+package d.garoz.main;
+
+public class mainMailTuit {
+
+}
---
+++ schedule_tuit/src/d/garoz/main/MainScheduleTuit.java
@@ -0,0 +1,13 @@
+package d.garoz.main;
+
+public class MainScheduleTuit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
